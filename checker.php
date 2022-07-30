fa078b81668fa79417ebd49841b318875e6d296d
byIx <40878600+byIx@users.noreply.github.com>
2022-07-30 22:47:16 +0600

    Update checker.php

--- checker.php
+++ checker.php
@@ -1,1 +1,1 @@
-{ "version":"2.5.0", "longversion":25000, "released":1658833200, "updateurl":"https:\/\/ipbmafia.ru\/files\/file\/2491-k4-info-to-postcontainer\/" }
+{ "version":"2.6.0", "longversion":26000, "released":1659200400, "updateurl":"https:\/\/ipbmafia.ru\/files\/file\/2491-k4-info-to-postcontainer\/" }
