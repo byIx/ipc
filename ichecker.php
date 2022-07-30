b638af006605b4665440aef4ecc13b0443c5d559
byIx <40878600+byIx@users.noreply.github.com>
2022-07-30 22:47:49 +0600

    Update ichecker.php

--- ichecker.php
+++ ichecker.php
@@ -1,1 +1,1 @@
-{ "version":"2.6.0", "longversion":26000, "released":1659200400, "updateurl":"https:\/\/forum.invisionize.pl\/files\/file\/840-k4-info-to-postcontainer\/" }
+{ "version":"2.6.0", "longversion":26000, "released":1659193200, "updateurl":"https:\/\/forum.invisionize.pl\/files\/file\/840-k4-info-to-postcontainer\/" }
